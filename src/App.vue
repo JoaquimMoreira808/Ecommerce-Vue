aa5dc80433387a4357b9e40f5b988dc9f3a2bd71
Drereis <andreluisdasilvareis03@gmail.com>
2025-05-30 21:13:37 -0300

    Inicio do footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import Navbar from "./components/FooterComponent.vue";
 </script>
 
 <template>
-  
+  <router-view></router-view>
 </template>
 
-<style>
-</style>
+<style></style>
